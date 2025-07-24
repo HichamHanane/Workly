d5a858a038fb841e588b3b3bd0fa5a600c306e05
HichamHanane <hichamhanane2000@gmail.com>
2025-07-24 16:22:18 +0200

    add section gallery

--- index.js
+++ index.js
@@ -24,9 +24,52 @@ remove.onclick = () => {
 const about_link = document.getElementById('about_us-mobile');
 
 about_link.onclick = () => {
-    
+
     responsive_menu.classList.remove('respo-menu-animation');
     responsive_menu.classList.add('respo-menu-animation-out');
     responsive_menu.style.display = "none";
 }
 
+
+
+var splide = new Splide('#main-carousel', {
+    pagination: false,
+    arrows:true
+    
+    
+});
+
+
+var thumbnails = document.getElementsByClassName('thumbnail');
+var current;
+
+
+for (var i = 0; i < thumbnails.length; i++) {
+    initThumbnail(thumbnails[i], i);
+}
+
+
+function initThumbnail(thumbnail, index) {
+    thumbnail.addEventListener('click', function () {
+        splide.go(index);
+    });
+}
+
+
+splide.on('mounted move', function () {
+    var thumbnail = thumbnails[splide.index];
+
+
+    if (thumbnail) {
+        if (current) {
+            current.classList.remove('is-active');
+        }
+
+
+        thumbnail.classList.add('is-active');
+        current = thumbnail;
+    }
+});
+
+
+splide.mount();
